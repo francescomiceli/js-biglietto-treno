a744ff6078cc9e4c654921e8ee01ab141549b189
francesco.miceli <francescomiceli@outlook.com>
2022-08-23 16:10:45 +0200

    ultimo aggiornamento

--- myjs.js
+++ myjs.js
@@ -1,14 +1,14 @@
 let anni = prompt('Quanti anni hai');
 let km = prompt('Quanti km vuoi percorrere?');
 let prezzo = 0.21;
-let sconto_20 = 20;
-let sconto_40 = 40;
+const sconto_20 = 20;
+const sconto_40 = 40;
 
 console.log(anni, 'Anni');
 console.log(km, 'Km');
 
 if (anni <= 18) {
-    document.write('Il tuo biglietto costa ' + km * prezzo - sconto_20, ' €');
+    document.write(' Hai diritto al 20% di sconto. Il tuo biglietto costa ' + km * prezzo, ' €');
 }
 
 // document.write('Il tuo biglietto costa ' + km * prezzo, ' €');
