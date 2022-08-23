c0330ee9fdfbaa4ad76ba33e15884992c804d7b4
francesco.miceli <francescomiceli@outlook.com>
2022-08-23 16:40:30 +0200

    Esercizio completo!

--- myjs.js
+++ myjs.js
@@ -1,14 +1,27 @@
 let anni = prompt('Quanti anni hai');
 let km = prompt('Quanti km vuoi percorrere?');
 let prezzo = 0.21;
-const sconto_20 = 20;
-const sconto_40 = 40;
+let prezzo_biglietto = prezzo * km;
+const sconto_20 = 0.2;
+const sconto_40 = 0.4;
 
-console.log(anni, 'Anni');
-console.log(km, 'Km');
 
 if (anni <= 18) {
-    document.write(' Hai diritto al 20% di sconto. Il tuo biglietto costa ' + km * prezzo, ' €');
+    let sconto = prezzo_biglietto * sconto_20;
+    let prezzo_scontato = prezzo_biglietto - sconto;
+    document.write(' Hai diritto al 20% di sconto. Il tuo biglietto costa ' + prezzo_scontato, ' € invece che ' + prezzo_biglietto, ' €');
+}
+
+else if (anni >= 65){
+    let sconto = prezzo_biglietto * sconto_40;
+    let prezzo_scontato = prezzo_biglietto - sconto;
+    document.write(' Hai diritto al 40% di sconto. Il tuo biglietto costa ' + prezzo_scontato, ' € invece che ' + prezzo_biglietto, ' €');
+
+}
+
+else {
+    document.write('Il tuo biglietto costa ' + km * prezzo, ' €');
+
 }
 
 // document.write('Il tuo biglietto costa ' + km * prezzo, ' €');
